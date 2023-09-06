332ad2dde17e4d140cf2fd7f26ce6156eeb2d406
LillKonst <konstlillkristine@gmail.com>
2023-09-06 10:04:34 +0200

    added product info

--- js/details.js
+++ js/details.js
@@ -3,6 +3,13 @@ function getJacketIdFromQuery() {
     return urlParams.get("id");
 }
 
+// fix this function
+function sizeSelection(parameter) {
+    select = document.getElementById("size-container").value = parameter;
+}
+  
+
+
 async function fetchJacketDetail() {
     const jacketId = getJacketIdFromQuery();
     if (!jacketId) {
@@ -16,10 +23,27 @@ const jacketDetail = await response.json();
 
 const jacketDetailContainer = document.getElementById("jacket-details");
 
-
+// fix the size buttons
 jacketDetailContainer.innerHTML = ""; 
-jacketDetailContainer.innerHTML += `<h1>${jacketDetail.title}</h1>`;
+jacketDetailContainer.innerHTML += `<div class="grid-for-product-info">
+<img src="${jacketDetail.image}" class="product-pic" alt="${jacketDetail.description}">
+<div class="grid-con-prod-2">
+<h1>${jacketDetail.title}</h1>
+<h2>${jacketDetail.price} NOK</h2>
+<p>${jacketDetail.description}</p>
 
+<div class="size-container"> 
+          <button id="XS" class="size-button" onclick=buttonSelection("XS")>${jacketDetail.sizes}</button>
+          <button id="S" class="size-button" onclick=buttonSelection("S")>${jacketDetail.sizes}</button>
+          <button id="M" class="size-button" onclick=buttonSelection("M")>${jacketDetail.sizes}</button>
+          <button id="L" class="size-button" onclick=buttonSelection("L")>${jacketDetail.sizes}</button>
+          <button id="XL" class="size-button" onclick=buttonSelection("XL")>${jacketDetail.sizes}</button>
+          <button id="XXL" class="size-button" onclick=buttonSelection("XXL")>${jacketDetail.sizes}</button>
+        </div>
+
+<a href="#" class="add-to-button" id="shoppingbag" onclick="changeLocation();return false;"> ADD TO BAG</a>
+</div>
+</div>`;
 }
 
 
